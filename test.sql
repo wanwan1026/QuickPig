0e89a97c61aa9e1ae3f03e4bb29e844785727633
Ricetia <babapass742@gmail.com>
2021-08-03 00:53:21 +0800

    del test

--- test.sql
+++
@@ -1,4 +0,0 @@
--- SELECT * FROM `quickpig`.`menu`;
-/* SELECT `name`,`price`,`image` FROM `menu` WHERE class='MM'; */
--- SELECT count(*) FROM `quickpig`.`menu` WHERE time >= '2021-07-15' and time < '2021-07-16';
-SELECT * FROM `quickpig`.`pick`;
